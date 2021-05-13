eb81669582dabef16925fa00324766ef0bf0fde6
Violetta Mamykina <violetta-1998m@mail.ru>
2021-05-13 13:14:14 +0300

    initial commit

---
+++ app/src/androidTest/java/com/example/navigationgraph/ExampleInstrumentedTest.kt
@@ -0,0 +1,120 @@
+package com.example.navigationgraph
+
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.uiautomator.UiDevice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class NavigationTest {
+//    @get:Rule
+//    val rule = activityScenarioRule<MainActivity>()
+//
+//    @Test
+//    fun forFirstScreen() {
+//        val uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+//        Espresso.onView(ViewMatchers.withId(R.id.toSecond)).perform(ViewActions.click())
+//        Espresso.onView(ViewMatchers.withId(R.id.second_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.pressBack()
+//        Espresso.onView(ViewMatchers.withId(R.id.first_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext())
+//        Espresso.onView(ViewMatchers.withText(R.string.to_about)).perform(ViewActions.click())
+//        Espresso.onView(ViewMatchers.withId(R.id.about_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.pressBack()
+//        Espresso.onView(ViewMatchers.withId(R.id.first_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        uiDevice.setOrientationLeft()
+//        Espresso.onView(ViewMatchers.withId(R.id.first_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.pressBackUnconditionally()
+//        val state = rule.scenario.state
+//        assertEquals("DESTROYED", state.toString())
+//    }
+//
+//    @Test
+//    fun forSecondScreen() {
+//        val uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+//        Espresso.onView(ViewMatchers.withId(R.id.toSecond)).perform(ViewActions.click())
+//        Espresso.onView(ViewMatchers.withId(R.id.toFirst)).perform(ViewActions.click())
+//        Espresso.onView(ViewMatchers.withId(R.id.first_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.onView(ViewMatchers.withId(R.id.toSecond)).perform(ViewActions.click())
+//        Espresso.onView(ViewMatchers.withId(R.id.toThird)).perform(ViewActions.click())
+//        Espresso.onView(ViewMatchers.withId(R.id.third_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.pressBack()
+//        Espresso.onView(ViewMatchers.withId(R.id.second_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext())
+//        Espresso.onView(ViewMatchers.withText(R.string.to_about)).perform(ViewActions.click())
+//        Espresso.onView(ViewMatchers.withId(R.id.about_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.pressBack()
+//        Espresso.onView(ViewMatchers.withId(R.id.second_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        uiDevice.setOrientationLeft()
+//        Espresso.onView(ViewMatchers.withId(R.id.second_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.pressBack()
+//        Espresso.onView(ViewMatchers.withId(R.id.first_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.pressBackUnconditionally()
+//        val state = rule.scenario.state
+//        assertEquals("DESTROYED", state.toString())
+//    }
+//    @Test
+//    fun forThirdScreen() {
+//        val uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+//        Espresso.onView(ViewMatchers.withId(R.id.toSecond)).perform(ViewActions.click())
+//        Espresso.onView(ViewMatchers.withId(R.id.toThird)).perform(ViewActions.click())
+//        Espresso.onView(ViewMatchers.withId(R.id.toFirstOther)).perform(ViewActions.click())
+//        Espresso.onView(ViewMatchers.withId(R.id.first_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.onView(ViewMatchers.withId(R.id.toSecond)).perform(ViewActions.click())
+//        Espresso.onView(ViewMatchers.withId(R.id.toThird)).perform(ViewActions.click())
+//        Espresso.onView(ViewMatchers.withId(R.id.toSecondOther)).perform(ViewActions.click())
+//        Espresso.onView(ViewMatchers.withId(R.id.second_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.onView(ViewMatchers.withId(R.id.toThird)).perform(ViewActions.click())
+//        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext())
+//        Espresso.onView(ViewMatchers.withText(R.string.to_about)).perform(ViewActions.click())
+//        Espresso.onView(ViewMatchers.withId(R.id.about_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.pressBack()
+//        Espresso.onView(ViewMatchers.withId(R.id.third_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        uiDevice.setOrientationLeft()
+//        Espresso.onView(ViewMatchers.withId(R.id.third_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.pressBack()
+//        Espresso.onView(ViewMatchers.withId(R.id.second_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.pressBack()
+//        Espresso.onView(ViewMatchers.withId(R.id.first_screen))
+//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+//        Espresso.pressBackUnconditionally()
+//        val state = rule.scenario.state
+//        assertEquals("DESTROYED", state.toString())
+//    }
+}
+
+
