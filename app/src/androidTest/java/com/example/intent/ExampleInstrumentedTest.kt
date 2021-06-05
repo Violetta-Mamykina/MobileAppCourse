2f36fd379972894296a86a42c34e6e13a7051cad
Violetta Mamykina <violetta-1998m@mail.ru>
2021-06-05 15:36:59 +0300

    intent

--- app/src/androidTest/java/com/example/intent/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/intent/ExampleInstrumentedTest.kt
@@ -8,7 +8,7 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-//import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiDevice
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -23,123 +23,123 @@ import org.junit.Rule
  */
 @RunWith(AndroidJUnit4::class)
 class NavigationTest {
-//    private val uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-//
-//    private fun currentScreen(id: Int) {
-//        Espresso.onView(ViewMatchers.withId(id))
-//            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
-//    }
-//
-//    private fun toScreen(idButton: Int, idScreen: Int) {
-//        Espresso.onView(ViewMatchers.withId(idButton)).perform(ViewActions.click())
-//        currentScreen(idScreen)
-//    }
-///*
-//    private fun toAboutNavigationDrawer(idScreen: Int, idNav: Int) {
-//        Espresso.onView(ViewMatchers.withId(idScreen)).perform(DrawerActions.open())
-//        Espresso.onView(ViewMatchers.withId(idNav))
-//            .perform(NavigationViewActions.navigateTo(R.id.to_about))
-//        currentScreen(R.id.about_screen)
-//    }
-//
-//    private fun toAboutBottomNavigation() {
-//        toScreen(R.id.bottom_navigation, R.id.about_screen)
-//    }
-// */
-//
-//    private fun toAboutOptionsMenu() {
-//        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext())
-//        Espresso.onView(ViewMatchers.withText(R.string.to_about)).perform(ViewActions.click())
-//        currentScreen(R.id.about_screen)
-//    }
-//
-//    private fun toEnd() {
-//        Espresso.pressBackUnconditionally()
-//        val state = rule.scenario.state
-//        assertEquals("DESTROYED", state.toString())
-//    }
-//
-//    @get:Rule
-//    val rule = activityScenarioRule<MainActivity>()
-//
-//    @Test
-//    fun forFirstScreen() {
-//        toScreen(R.id.to_second, R.id.second_screen)
-//        Espresso.pressBack()
-//        currentScreen(R.id.first_screen)
-//
-//        //for Navigation Drawer
-////        toAboutNavigationDrawer(R.id.first_screen, R.id.nav_view_first)
-//
-//        //for Bottom Navigation
-////        toAboutBottomNavigation()
-//
-//        //for Options Menu
-//        toAboutOptionsMenu()
-//
-//        Espresso.pressBack()
-//        currentScreen(R.id.first_screen)
-//        uiDevice.setOrientationLeft()
-//        currentScreen(R.id.first_screen)
-//        toEnd()
-//    }
-//
-//    @Test
-//    fun forSecondScreen() {
-//        toScreen(R.id.to_second, R.id.second_screen)
-//        toScreen(R.id.to_first, R.id.first_screen)
-//        toScreen(R.id.to_second, R.id.second_screen)
-//        toScreen(R.id.to_third, R.id.third_screen)
-//        Espresso.pressBack()
-//        currentScreen(R.id.second_screen)
-//
-//        //for Navigation Drawer
-////        toAboutNavigationDrawer(R.id.second_screen, R.id.nav_view_second)
-//
-//        //for Bottom Navigation
-////        toAboutBottomNavigation()
-//
-//        //for Options Menu
-//        toAboutOptionsMenu()
-//
-//        Espresso.pressBack()
-//        currentScreen(R.id.second_screen)
-//        uiDevice.setOrientationLeft()
-//        currentScreen(R.id.second_screen)
-//        Espresso.pressBack()
-//        currentScreen(R.id.first_screen)
-//        toEnd()
-//    }
-//
-//    @Test
-//    fun forThirdScreen() {
-//        toScreen(R.id.to_second, R.id.second_screen)
-//        toScreen(R.id.to_third, R.id.third_screen)
-//        toScreen(R.id.to_first_t, R.id.first_screen)
-//        toScreen(R.id.to_second, R.id.second_screen)
-//        toScreen(R.id.to_third, R.id.third_screen)
-//        toScreen(R.id.to_second_t, R.id.second_screen)
-//        toScreen(R.id.to_third, R.id.third_screen)
-//
-//        //for Navigation Drawer
-////        toAboutNavigationDrawer(R.id.third_screen, R.id.nav_view_third)
-//
-//        //for Bottom Navigation
-////        toAboutBottomNavigation()
-//
-//        //for Options Menu
-//        toAboutOptionsMenu()
-//
-//        Espresso.pressBack()
-//        currentScreen(R.id.third_screen)
-//        uiDevice.setOrientationLeft()
-//        currentScreen(R.id.third_screen)
-//        Espresso.pressBack()
-//        currentScreen(R.id.second_screen)
-//        Espresso.pressBack()
-//        currentScreen(R.id.first_screen)
-//        toEnd()
-//    }
+    private val uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+
+    private fun currentScreen(id: Int) {
+        Espresso.onView(ViewMatchers.withId(id))
+            .check(ViewAssertions.matches(ViewMatchers.isCompletelyDisplayed()))
+    }
+
+    private fun toScreen(idButton: Int, idScreen: Int) {
+        Espresso.onView(ViewMatchers.withId(idButton)).perform(ViewActions.click())
+        currentScreen(idScreen)
+    }
+/*
+    private fun toAboutNavigationDrawer(idScreen: Int, idNav: Int) {
+        Espresso.onView(ViewMatchers.withId(idScreen)).perform(DrawerActions.open())
+        Espresso.onView(ViewMatchers.withId(idNav))
+            .perform(NavigationViewActions.navigateTo(R.id.to_about))
+        currentScreen(R.id.about_screen)
+    }
+
+    private fun toAboutBottomNavigation() {
+        toScreen(R.id.bottom_navigation, R.id.about_screen)
+    }
+ */
+
+    private fun toAboutOptionsMenu() {
+        Espresso.openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext())
+        Espresso.onView(ViewMatchers.withText(R.string.to_about)).perform(ViewActions.click())
+        currentScreen(R.id.about_screen)
+    }
+
+    private fun toEnd() {
+        Espresso.pressBackUnconditionally()
+        val state = rule.scenario.state
+        assertEquals("DESTROYED", state.toString())
+    }
+
+    @get:Rule
+    val rule = activityScenarioRule<MainActivity>()
+
+    @Test
+    fun forFirstScreen() {
+        toScreen(R.id.to_second, R.id.second_screen)
+        Espresso.pressBack()
+        currentScreen(R.id.first_screen)
+
+        //for Navigation Drawer
+//        toAboutNavigationDrawer(R.id.first_screen, R.id.nav_view_first)
+
+        //for Bottom Navigation
+//        toAboutBottomNavigation()
+
+        //for Options Menu
+        toAboutOptionsMenu()
+
+        Espresso.pressBack()
+        currentScreen(R.id.first_screen)
+        uiDevice.setOrientationLeft()
+        currentScreen(R.id.first_screen)
+        toEnd()
+    }
+
+    @Test
+    fun forSecondScreen() {
+        toScreen(R.id.to_second, R.id.second_screen)
+        toScreen(R.id.to_first, R.id.first_screen)
+        toScreen(R.id.to_second, R.id.second_screen)
+        toScreen(R.id.to_third, R.id.third_screen)
+        Espresso.pressBack()
+        currentScreen(R.id.second_screen)
+
+        //for Navigation Drawer
+//        toAboutNavigationDrawer(R.id.second_screen, R.id.nav_view_second)
+
+        //for Bottom Navigation
+//        toAboutBottomNavigation()
+
+        //for Options Menu
+        toAboutOptionsMenu()
+
+        Espresso.pressBack()
+        currentScreen(R.id.second_screen)
+        uiDevice.setOrientationLeft()
+        currentScreen(R.id.second_screen)
+        Espresso.pressBack()
+        currentScreen(R.id.first_screen)
+        toEnd()
+    }
+
+    @Test
+    fun forThirdScreen() {
+        toScreen(R.id.to_second, R.id.second_screen)
+        toScreen(R.id.to_third, R.id.third_screen)
+        toScreen(R.id.to_first_t, R.id.first_screen)
+        toScreen(R.id.to_second, R.id.second_screen)
+        toScreen(R.id.to_third, R.id.third_screen)
+        toScreen(R.id.to_second_t, R.id.second_screen)
+        toScreen(R.id.to_third, R.id.third_screen)
+
+        //for Navigation Drawer
+//        toAboutNavigationDrawer(R.id.third_screen, R.id.nav_view_third)
+
+        //for Bottom Navigation
+//        toAboutBottomNavigation()
+
+        //for Options Menu
+        toAboutOptionsMenu()
+
+        Espresso.pressBack()
+        currentScreen(R.id.third_screen)
+        uiDevice.setOrientationLeft()
+        currentScreen(R.id.third_screen)
+        Espresso.pressBack()
+        currentScreen(R.id.second_screen)
+        Espresso.pressBack()
+        currentScreen(R.id.first_screen)
+        toEnd()
+    }
 }
 
 
