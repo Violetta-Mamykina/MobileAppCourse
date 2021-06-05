dc4770e2d14550d7923fb30fa022397a59eb28b8
Violetta Mamykina <violetta-1998m@mail.ru>
2021-06-05 14:52:06 +0300

    Glide

--- app/src/main/java/com/example/downloadimagelib/MainActivity.kt
+++ app/src/main/java/com/example/downloadimagelib/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.downloadimagelib
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Glide.with(applicationContext)
+            .load("https://i.artfile.ru/4500x3514_903167_[www.ArtFile.ru].jpg")
+            .into(findViewById(R.id.dow_image))
+
     }
 }
+
+
